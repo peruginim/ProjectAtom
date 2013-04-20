afe8ac5f05fc4f7ad25e3fb2c9f2689990c78c42
Niraj Venkat <venkatn@purdue.edu>
2013-04-20 16:05:50 -0400

    Removed bar from top of splash screen. Added music option

--- Atoms/src/cs252/lab6/atoms/Splash.java
+++ Atoms/src/cs252/lab6/atoms/Splash.java
@@ -22,7 +22,7 @@ public class Splash extends Activity{
 		Thread timer = new Thread(){
 			public void run(){
 				try{
-					sleep(5000);
+					sleep(3000);
 				}
 				catch(InterruptedException e){
 					e.printStackTrace();
