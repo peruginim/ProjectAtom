4339f90eddf5371802cc07f10e1df97a2c0f3d87
Niraj Venkat <venkatn@purdue.edu>
2013-04-20 16:04:45 -0400

    Splash screen integrated through manifest. Stays for 3s

--- Atoms/src/cs252/lab6/atoms/Splash.java
+++ Atoms/src/cs252/lab6/atoms/Splash.java
@@ -5,22 +5,24 @@ import android.app.Activity;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.Window;
 
 public class Splash extends Activity{
-	//MediaPlayer damnson;
+	//MediaPlayer music;
 	
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.splash);
-		//damnson = MediaPlayer.create(Splash.this, R.raw.music);
-		//damnson.setLooping(true);
-		//damnson.start();
+		//music = MediaPlayer.create(Splash.this, R.raw.music);
+		//music.setLooping(true);
+		//music.start();
 		
 		Thread timer = new Thread(){
 			public void run(){
 				try{
-					sleep(500);
+					sleep(5000);
 				}
 				catch(InterruptedException e){
 					e.printStackTrace();
