85223f983adc1128342c6bad921ca37bf838d851
Sam Dickson <scdickso@purdue.edu>
2013-04-24 21:13:36 -0400

    Basic functionality of multiplayer game.

--- Atoms/src/cs252/lab6/atoms/Menu.java
+++ Atoms/src/cs252/lab6/atoms/Menu.java
@@ -1,5 +1,7 @@
 package cs252.lab6.atoms;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 import android.app.ListActivity;
@@ -61,6 +63,14 @@ public class Menu extends ListActivity{
 			try
 			{
 				Game.s = new Socket(Game.SERVER_PATH, Game.SERVER_PORT);
+				BufferedReader in = new BufferedReader(new InputStreamReader(Game.s.getInputStream()));
+				String player = "";
+				while((player = in.readLine()) != null)
+				{
+					Game.player = Integer.parseInt(player);
+					break;
+				}
+
 			}
 			catch(Exception e)
 			{
