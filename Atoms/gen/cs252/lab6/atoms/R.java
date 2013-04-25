7e288b00d2a412861f6820ae70926d0678b680a3
Michael Perugini <11peruginiM@gmail.com>
2013-04-24 20:18:08 -0400

    Added settings.java and settings.xml

--- Atoms/gen/cs252/lab6/atoms/R.java
+++ Atoms/gen/cs252/lab6/atoms/R.java
@@ -26,19 +26,34 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int ic_launcher_web=0x7f020001;
     }
     public static final class id {
+        public static final int LinearLayout1=0x7f070002;
+        public static final int button1=0x7f070010;
+        public static final int checkBox1=0x7f070005;
+        public static final int checkBox2=0x7f070006;
+        public static final int checkBox3=0x7f070008;
+        public static final int checkBox4=0x7f070009;
+        public static final int checkBox5=0x7f07000b;
+        public static final int checkBox6=0x7f07000c;
+        public static final int checkBox7=0x7f07000e;
+        public static final int checkBox8=0x7f07000f;
         public static final int gameview=0x7f070000;
-        public static final int imageView1=0x7f070002;
+        public static final int imageView1=0x7f070011;
         public static final int listview=0x7f070001;
+        public static final int player1=0x7f070004;
+        public static final int player2=0x7f070007;
+        public static final int player3=0x7f07000a;
+        public static final int player4=0x7f07000d;
         public static final int textView1=0x7f070003;
-        public static final int textView2=0x7f070004;
-        public static final int textView3=0x7f070005;
-        public static final int textView4=0x7f070006;
-        public static final int textView5=0x7f070007;
+        public static final int textView2=0x7f070012;
+        public static final int textView3=0x7f070013;
+        public static final int textView4=0x7f070014;
+        public static final int textView5=0x7f070015;
     }
     public static final class layout {
         public static final int activity_game=0x7f030000;
         public static final int match=0x7f030001;
-        public static final int splash=0x7f030002;
+        public static final int settings=0x7f030002;
+        public static final int splash=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
