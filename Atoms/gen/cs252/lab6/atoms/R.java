6bae844fe46721024bb7506b21c2e02d5ab28fd0
Niraj Venkat <venkatn@purdue.edu>
2013-04-25 00:40:11 -0400

    Commit

--- Atoms/gen/cs252/lab6/atoms/R.java
+++ Atoms/gen/cs252/lab6/atoms/R.java
@@ -8,6 +8,9 @@
 package cs252.lab6.atoms;
 
 public final class R {
+    public static final class array {
+        public static final int colors=0x7f060000;
+    }
     public static final class attr {
         /** <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
 or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
@@ -26,28 +29,32 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int ic_launcher_web=0x7f020001;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f070002;
-        public static final int back_button=0x7f070010;
-        public static final int checkBox1=0x7f070005;
-        public static final int checkBox2=0x7f070006;
-        public static final int checkBox3=0x7f070008;
-        public static final int checkBox4=0x7f070009;
-        public static final int checkBox5=0x7f07000b;
-        public static final int checkBox6=0x7f07000c;
-        public static final int checkBox7=0x7f07000e;
-        public static final int checkBox8=0x7f07000f;
-        public static final int gameview=0x7f070000;
-        public static final int imageView1=0x7f070011;
-        public static final int listview=0x7f070001;
-        public static final int player1=0x7f070004;
-        public static final int player2=0x7f070007;
-        public static final int player3=0x7f07000a;
-        public static final int player4=0x7f07000d;
-        public static final int textView1=0x7f070003;
-        public static final int textView2=0x7f070012;
-        public static final int textView3=0x7f070013;
-        public static final int textView4=0x7f070014;
-        public static final int textView5=0x7f070015;
+        public static final int LinearLayout1=0x7f080002;
+        public static final int back_button=0x7f080014;
+        public static final int checkBox1=0x7f080005;
+        public static final int checkBox2=0x7f080006;
+        public static final int checkBox3=0x7f080009;
+        public static final int checkBox4=0x7f08000a;
+        public static final int checkBox5=0x7f08000d;
+        public static final int checkBox6=0x7f08000e;
+        public static final int checkBox7=0x7f080011;
+        public static final int checkBox8=0x7f080012;
+        public static final int gameview=0x7f080000;
+        public static final int imageView1=0x7f080015;
+        public static final int listview=0x7f080001;
+        public static final int player1=0x7f080004;
+        public static final int player2=0x7f080008;
+        public static final int player3=0x7f08000c;
+        public static final int player4=0x7f080010;
+        public static final int spinner1=0x7f080007;
+        public static final int spinner2=0x7f08000b;
+        public static final int spinner3=0x7f08000f;
+        public static final int spinner4=0x7f080013;
+        public static final int textView1=0x7f080003;
+        public static final int textView2=0x7f080016;
+        public static final int textView3=0x7f080017;
+        public static final int textView4=0x7f080018;
+        public static final int textView5=0x7f080019;
     }
     public static final class layout {
         public static final int activity_game=0x7f030000;
@@ -57,6 +64,7 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     }
     public static final class string {
         public static final int app_name=0x7f050000;
+        public static final int color_prompt=0x7f050004;
         public static final int dummy_button=0x7f050001;
         public static final int dummy_content=0x7f050002;
         public static final int players_header=0x7f050003;
@@ -86,15 +94,15 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
-        public static final int ButtonBar=0x7f060003;
-        public static final int ButtonBarButton=0x7f060004;
-        public static final int FullscreenActionBarStyle=0x7f060005;
-        public static final int FullscreenTheme=0x7f060002;
+        public static final int AppTheme=0x7f070001;
+        public static final int ButtonBar=0x7f070003;
+        public static final int ButtonBarButton=0x7f070004;
+        public static final int FullscreenActionBarStyle=0x7f070005;
+        public static final int FullscreenTheme=0x7f070002;
     }
     public static final class styleable {
         /** 
